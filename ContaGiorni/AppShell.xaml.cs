27667d97203e6cc2b16f30782c0124464c052b21
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2024-07-19 19:19:50 +0200

    start implemeting

---
+++ ContaGiorni/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ContaGiorni;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ContaGiorni/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace ContaGiorni;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		DateTime dateNow = dateFrom.Date;
+		string days = daysToAdd.Text;
+		dateTo.Date = dateNow.AddDays(Int32.Parse(days));
+	}
+}
+
